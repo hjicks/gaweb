1a1c99c5e6aeb91514f0a18daef054e41042c017
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-16 16:01:38 +0330

    change channel and group relation, add foreign key from user, add namespace

--- MASsenger/Channel.cs
+++ MASsenger/Channel.cs
@@ -1,18 +1,22 @@
-﻿public class Channel
+﻿namespace MASsenger
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = "MASsenger Channel";
-    public string? Description { get; set; }
-    public string? Photo { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public bool IsPublic { get; set; } = false;
-    public int MembersCount { get; set; } = 1;
+    public class Channel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "MASsenger Channel";
+        public string? Description { get; set; }
+        public string? Photo { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsPublic { get; set; } = false;
+        public int MembersCount { get; set; } = 1;
 
-    // Foreign key relationships
-    public string Creator { get; set; } // User foreign key
+        // Foreign key relationships
+        public User Creator { get; set; }
+        public Group? Group { get; set; }
 
 
-    // Navigation properties
-    public ICollection<Group> Groups { get; set; } = new List<Group>();
-    public ICollection<Bot> Bots { get; set; } = new List<Bot>();
+        // Navigation properties
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Bot> Bots { get; set; } = new List<Bot>();
+    }
 }
