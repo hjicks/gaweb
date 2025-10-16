cff00a7644f980da7a5c090324ea65a28d527aea
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-16 22:04:07 +0330

    remove channel relation with group

--- MASsenger/Channel.cs
+++ MASsenger/Channel.cs
@@ -10,10 +10,6 @@
         public bool IsPublic { get; set; } = false;
         public int MembersCount { get; set; } = 1;
 
-        // Foreign key relationships
-        public int GroupId { get; set; }
-        public Group? Group { get; set; }
-
         // Navigation properties
         public ICollection<User> Members { get; set; } = new List<User>();
         public ICollection<Bot> Bots { get; set; } = new List<Bot>();
