0e0806c29560a1167553eab25657f34b7bb4ffd3
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-16 15:57:38 +0330

    add namespace

--- MASsenger/BaseUser.cs
+++ MASsenger/BaseUser.cs
@@ -1,10 +1,13 @@
-﻿public class BaseUser
+﻿namespace MASsenger
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = "MASsenger User";
-    public string? Username { get; set; } = null;
-    public string? Description { get; set; }
-    public string? ProfilePictureUrl { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public bool IsVerified { get; set; } = false;
+    public class BaseUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "MASsenger User";
+        public string? Username { get; set; } = null;
+        public string? Description { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsVerified { get; set; } = false;
+    }
 }
