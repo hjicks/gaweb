03bedca9b9f6fe9dc9e194911bcfb64219e4a6ed
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-23 21:30:06 +0330

    add dto for BaseMessage entity

---
+++ MASsenger.Core/Dto/BaseMessageDto.cs
@@ -0,0 +1,20 @@
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Core.Dto
+{
+    public class BaseMessageDto
+    {
+        public UInt64 Id;
+        public MessageType Type { get; set; }
+        public BaseUser Sender { get; set; } = null!;
+        public BaseChat Destination { get; set; } = null!;
+        public DateTime SentTime { get; set; }
+        public string Text { get; set; } = null!;
+    }
+}
