8efb5dba6056f33d8778517edd8ea41d2219f179
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-23 21:20:13 +0330

    add repository for BaseMessage entity

---
+++ MASsenger.Core/Interfaces/IBaseMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using MASsenger.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Core.Interfaces
+{
+    public interface IBaseMessageRepository
+    {
+        Task<IEnumerable<BaseMessage>> GetBaseMessagesAsync();
+    }
+}
---
+++ MASsenger.Infrastracture/Repositories/BaseMessageRepository.cs
@@ -0,0 +1,26 @@
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MASsenger.Infrastracture.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Infrastracture.Repositories
+{
+    public class BaseMessageRepository : IBaseMessageRepository
+    {
+        private readonly MessengerDbContext _context;
+        public BaseMessageRepository(MessengerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BaseMessage>> GetBaseMessagesAsync()
+        {
+            return await _context.BaseMessages.ToListAsync();
+        }
+    }
+}
