c031a080c875e6360e46c605025bd19345ecd764
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 00:46:13 +0330

    add controller for BaseMessage entity

---
+++ MASsenger.Api/Controllers/BaseMessagesController.cs
@@ -0,0 +1,35 @@
+﻿using MASsenger.Core.Dto;
+using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MASsenger.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseMessagesController : ControllerBase
+    {
+        private readonly IBaseMessageRepository _baseMessageRepository;
+        public BaseMessagesController(IBaseMessageRepository baseMessageRepository)
+        {
+            _baseMessageRepository = baseMessageRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<BaseMessage>))]
+        public async Task<IActionResult> GetBaseMessages()
+        {
+            var baseMessages = await _baseMessageRepository.GetBaseMessagesAsync();
+            var baseMessagesDtos = baseMessages.Select(u => new BaseMessageDto
+            {
+                Id = u.Id,
+                Sender = u.Sender,
+                Destination = u.Destination,
+                SentTime = u.SentTime,
+                Text = u.Text
+            }).ToList();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            return Ok(baseMessagesDtos);
+        }
+    }
+}
