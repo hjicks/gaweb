dc80fa70774a5d12e636819317f48fa64def3350
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 22:55:06 +0330

    add BaseChat repository

---
+++ MASsenger.Infrastracture/Repositories/BaseChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using MASsenger.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Infrastracture.Repositories
+{
+    public class BaseChatRepository : IBaseChatRepository
+    {
+    }
+}
