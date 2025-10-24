415745b6eee3ac6aa22ad00f987a4c4619f2e97d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 22:55:27 +0330

    add BaseChat controller

---
+++ MASsenger.Api/Controllers/BaseChatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MASsenger.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseChatController : ControllerBase
+    {
+    }
+}
