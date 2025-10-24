2abb92084eb242ac9efa56cdac74729c92703c4d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-25 00:21:51 +0330

    add read and create operations to repository

--- MASsenger.Core/Interfaces/IBaseChatRepository.cs
+++ MASsenger.Core/Interfaces/IBaseChatRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MASsenger.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace MASsenger.Core.Interfaces
 {
     public interface IBaseChatRepository
     {
+        Task<BaseChat?> GetBaseChatByIdAsync(UInt64 chatId);
+        Task<IEnumerable<ChannelGroupChat>> GetChannelGroupChatsAsync();
+        Task<ChannelGroupChat?> GetChannelGroupChatByIdAsync(UInt64 chatId);
+        Task<bool> AddChannelGroupChatAsync(ChannelGroupChat channelGroupChat, User owner);
+        Task<bool> Save();
     }
 }
--- MASsenger.Infrastracture/Repositories/BaseChatRepository.cs
+++ MASsenger.Infrastracture/Repositories/BaseChatRepository.cs
@@ -1,4 +1,7 @@
-﻿using MASsenger.Core.Interfaces;
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MASsenger.Infrastracture.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,37 @@ namespace MASsenger.Infrastracture.Repositories
 {
     public class BaseChatRepository : IBaseChatRepository
     {
+        private readonly MessengerDbContext _context;
+        public BaseChatRepository(MessengerDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<BaseChat?> GetBaseChatByIdAsync(UInt64 chatId)
+        {
+            return await _context.BaseChats.FirstOrDefaultAsync(c => c.Id == chatId);
+        }
+
+        public async Task<IEnumerable<ChannelGroupChat>> GetChannelGroupChatsAsync()
+        {
+            return await _context.ChannelGroupChats.ToListAsync();
+        }
+
+        public async Task<ChannelGroupChat?> GetChannelGroupChatByIdAsync(UInt64 chatId)
+        {
+            return await _context.ChannelGroupChats.FirstOrDefaultAsync(c => c.Id == chatId);
+        }
+
+        public Task<bool> AddChannelGroupChatAsync(ChannelGroupChat channelGroupChat, User owner)
+        {
+            channelGroupChat.Owner = owner;
+            _context.ChannelGroupChats.Add(channelGroupChat);
+            return Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0 ? true : false;
+        }
     }
 }
