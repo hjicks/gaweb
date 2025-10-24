6935b5f8c4d341bd9dbb4ae44966ace62fe1955b
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 21:47:01 +0330

    add bot dto

---
+++ MASsenger.Core/Dto/BotDto.cs
@@ -0,0 +1,13 @@
+﻿using MASsenger.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace MASsenger.Core.Dto
+{
+    public class BotDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+        public string Token { get; set; } = null!;
+    }
+}
