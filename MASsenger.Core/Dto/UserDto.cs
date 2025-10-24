562010f032d22c0d7319e2ebc66b71da25415176
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 22:50:08 +0330

    add User dto

---
+++ MASsenger.Core/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Core.Dto
+{
+    public class UserDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+    }
+}
