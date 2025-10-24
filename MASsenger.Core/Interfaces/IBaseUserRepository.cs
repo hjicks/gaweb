5f413623482b472a0f8f78ceb9dd3bbd5982db91
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-24 21:46:00 +0330

    add BaseUser repository and interface

---
+++ MASsenger.Core/Interfaces/IBaseUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using MASsenger.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Core.Interfaces
+{
+    public interface IBaseUserRepository
+    {
+        Task<IEnumerable<BaseUser>> GetBaseUsersAsync();
+        Task<User?> GetUserByIdAsync(UInt64 userId);
+        Task<bool> AddUserAsync(User user);
+        Task<bool> AddBotAsync(Bot bot, User user);
+        Task<bool> Save();
+    }
+}
---
+++ MASsenger.Infrastracture/Repositories/BaseUserRepository.cs
@@ -0,0 +1,50 @@
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MASsenger.Infrastracture.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Infrastracture.Repositories
+{
+    public class BaseUserRepository : IBaseUserRepository
+    {
+        private readonly MessengerDbContext _context;
+        public BaseUserRepository(MessengerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<BaseUser>> GetBaseUsersAsync()
+        {
+            return await _context.BaseUsers.ToListAsync();
+        }
+
+        public async Task<User?> GetUserByIdAsync(UInt64 userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public Task<bool> AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            return Save();
+        }
+
+        public Task<bool> AddBotAsync(Bot bot, User user)
+        {
+            bot.Owner = user;
+            _context.Bots.Add(bot);
+            return Save();
+        }
+
+        public async Task<bool> Save()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0 ? true : false;
+        }
+    }
+}
