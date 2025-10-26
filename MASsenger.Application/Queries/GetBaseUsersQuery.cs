55295ee05b6aa971de924ede878e16f8e75d9f3e
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-26 22:02:34 +0330

    add GetBaseUsersQuery to implement CQRS

---
+++ MASsenger.Application/Queries/GetBaseUsersQuery.cs
@@ -0,0 +1,25 @@
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Application.Queries
+{
+    public record GetBaseUsersQuery() : IRequest<IEnumerable<BaseUser>>;
+    public class GetBaseUsersHandler : IRequestHandler<GetBaseUsersQuery, IEnumerable<BaseUser>>
+    {
+        private readonly IBaseUserRepository _baseUserRepository;
+        public GetBaseUsersHandler(IBaseUserRepository baseUserRepository) 
+        {
+            _baseUserRepository = baseUserRepository;
+        }
+        public async Task<IEnumerable<BaseUser>> Handle(GetBaseUsersQuery request, CancellationToken cancellationToken)
+        {
+            return await _baseUserRepository.GetBaseUsersAsync();
+        }
+    }
+}
