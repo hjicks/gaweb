6e1f8e179a001daa24255669d3328100c478c84d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-26 22:04:11 +0330

    add AddUserCommand to implement CQRS

---
+++ MASsenger.Application/Commands/AddUserCommand.cs
@@ -0,0 +1,27 @@
+﻿using MASsenger.Core.Dto;
+using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MediatR;
+
+namespace MASsenger.Application.Commands
+{
+    public record AddUserCommand(UserDto user) : IRequest<bool>;
+    public class AddUserCommandHandler : IRequestHandler<AddUserCommand, bool>
+    {
+        private readonly IBaseUserRepository _baseUserRepository;
+        public AddUserCommandHandler(IBaseUserRepository baseUserRepository)
+        {
+            _baseUserRepository = baseUserRepository;
+        }
+        public Task<bool> Handle(AddUserCommand request, CancellationToken cancellationToken)
+        {
+            var newUser = new User
+            {
+                Name = request.user.Name,
+                Username = request.user.Username,
+                Description = request.user.Description
+            };
+            return _baseUserRepository.AddUserAsync(newUser);
+        }
+    }
+}
