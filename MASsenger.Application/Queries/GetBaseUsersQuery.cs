154545716701b496249971463fdace2c59f9219e
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-26 22:05:29 +0330

    remove unnecessary usings

--- MASsenger.Application/Queries/GetBaseUsersQuery.cs
+++ MASsenger.Application/Queries/GetBaseUsersQuery.cs
@@ -1,11 +1,6 @@
 ﻿using MASsenger.Core.Entities;
 using MASsenger.Core.Interfaces;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MASsenger.Application.Queries
 {
