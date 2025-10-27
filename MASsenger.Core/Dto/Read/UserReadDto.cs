cb4ee647bbe7d9e8fa7f9fe1bd9c2ae35ac6a316
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 21:06:07 +0330

    UserReadDto.cs: import

---
+++ MASsenger.Core/Dto/Read/UserReadDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MASsenger.Core.Dto.Read
+{
+    public class UserReadDto
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsVerified { get; set; }
+    }
+}
