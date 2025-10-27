2d37e36b0452943228f1bf9c9a277a92925dbfd1
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 18:52:32 +0330

    add dto for updating user

---
+++ MASsenger.Core/Dto/UserUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MASsenger.Core.Dto
+{
+    public class UserUpdateDto
+    {
+        public UInt64 Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+    }
+}
