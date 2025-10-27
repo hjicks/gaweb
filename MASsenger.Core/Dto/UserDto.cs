b14cd3313d82130f900382c42114adce424dc093
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 19:13:40 +0330

    add validation to user dto

--- MASsenger.Core/Dto/UserDto.cs
+++ MASsenger.Core/Dto/UserDto.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MASsenger.Core.Dto
 {
     public class UserDto
     {
+        [MinLength(1)]
+        [MaxLength(64)]
         public string Name { get; set; } = null!;
+        [MinLength(5)]
+        [MaxLength(32)]
+        [RegularExpression("^[a-zA-Z0-9_]+$")]
         public string? Username { get; set; }
+        [MaxLength(70)]
         public string? Description { get; set; }
     }
 }
