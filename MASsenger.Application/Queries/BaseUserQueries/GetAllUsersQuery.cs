e1c81a61911a2566f8712a6eb91c5675ed7a983c
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 21:42:33 +0330

    GetUsersQuery.cs: rename to GetAllUsersQuery

--- MASsenger.Application/Queries/BaseUserQueries/GetAllUsersQuery.cs
+++ MASsenger.Application/Queries/BaseUserQueries/GetAllUsersQuery.cs
@@ -4,17 +4,17 @@ using MediatR;
 
 namespace MASsenger.Application.Queries.BaseUserQueries
 {
-    public record GetUsersQuery() : IRequest<IEnumerable<User>>;
-    public class GetUsersCommandHandler : IRequestHandler<GetUsersQuery, IEnumerable<User>>
+    public record GetAllUsersQuery() : IRequest<IEnumerable<User>>;
+    public class GetUsersCommandHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<User>>
     {
         private readonly IBaseUserRepository _baseUserRepository;
         public GetUsersCommandHandler(IBaseUserRepository baseUserRepository) 
         {
             _baseUserRepository = baseUserRepository;
         }
-        public async Task<IEnumerable<User>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
         {
-            return await _baseUserRepository.GetUsersAsync();
+            return await _baseUserRepository.GetAllUsersAsync();
         }
     }
 }
