6ac69878c5d28f2f883fb67ce1d8ba2d3b2a1469
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 21:12:31 +0330

    GetUsersQuery.cs: import

---
+++ MASsenger.Application/Queries/GetUsersQuery.cs
@@ -0,0 +1,20 @@
+﻿using MASsenger.Core.Entities;
+using MASsenger.Core.Interfaces;
+using MediatR;
+
+namespace MASsenger.Application.Queries
+{
+    public record GetUsersQuery() : IRequest<IEnumerable<User>>;
+    public class GetUsersCommandHandler : IRequestHandler<GetUsersQuery, IEnumerable<User>>
+    {
+        private readonly IBaseUserRepository _baseUserRepository;
+        public GetUsersCommandHandler(IBaseUserRepository baseUserRepository) 
+        {
+            _baseUserRepository = baseUserRepository;
+        }
+        public async Task<IEnumerable<User>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            return await _baseUserRepository.GetUsersAsync();
+        }
+    }
+}
