8239ee972c9bb38e1771455ffc1ae7145f9b949d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 21:03:31 +0330

    rename user and bot dtos and move them to corresponding folder

---
+++ MASsenger.Core/Dto/Create/BotCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MASsenger.Core.Dto.Create
+{
+    public class BotCreateDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+        public string Token { get; set; } = null!;
+    }
+}
---
+++ MASsenger.Core/Dto/Update/UserUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MASsenger.Core.Dto.Update
+{
+    public class UserUpdateDto
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+    }
+}
