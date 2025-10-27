a0f5c9c6745816bbb8167aa15c65e8f56f09b3b3
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 21:40:21 +0330

    IBaseUserRepository.cs: rename GetUsersAsync definition, add GetAllBotsAsync and GetBotByIdAsync definitions

--- MASsenger.Core/Interfaces/IBaseUserRepository.cs
+++ MASsenger.Core/Interfaces/IBaseUserRepository.cs
@@ -4,11 +4,13 @@ namespace MASsenger.Core.Interfaces
 {
     public interface IBaseUserRepository
     {
-        Task<IEnumerable<User>> GetUsersAsync();
+        Task<IEnumerable<User>> GetAllUsersAsync();
         Task<User?> GetUserByIdAsync(UInt64 userId);
         Task<bool> AddUserAsync(User user);
         Task<bool> UpdateUserAsync(User user);
         Task<bool> DeleteUserAsync(User user);
+        Task<IEnumerable<Bot>> GetAllBotsAsync();
+        Task<Bot?> GetBotByIdAsync(UInt64 botId);
         Task<bool> AddBotAsync(Bot bot, User user);
         Task<bool> Save();
     }
