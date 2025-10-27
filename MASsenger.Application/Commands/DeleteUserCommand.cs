3a2c34e15eec4ef989634208b79113e0d71cddac
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-10-27 18:48:12 +0330

    add delete user command

---
+++ MASsenger.Application/Commands/DeleteUserCommand.cs
@@ -0,0 +1,25 @@
+﻿using MASsenger.Core.Enums;
+using MASsenger.Core.Interfaces;
+using MediatR;
+
+namespace MASsenger.Application.Commands
+{
+    public record DeleteUserCommand(UInt64 userId) : IRequest<TransactionResultType>;
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, TransactionResultType>
+    {
+        private readonly IBaseUserRepository _baseUserRepository;
+        public DeleteUserCommandHandler(IBaseUserRepository baseUserRepository)
+        {
+            _baseUserRepository = baseUserRepository;
+        }
+
+        public async Task<TransactionResultType> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _baseUserRepository.GetUserByIdAsync(request.userId);
+            if (user == null)
+                return TransactionResultType.ForeignKeyNotFound;
+            if (await _baseUserRepository.DeleteUserAsync(user)) return TransactionResultType.Done;
+            return TransactionResultType.SaveChangesError;
+        }
+    }
+}
