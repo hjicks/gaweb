c77c84ed6e59b8245f1646ee9131d010d8f714cb
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-08 11:17:57 +0330

    feat: add generic repository
    we can replace this repository with previous ones which decreases redundancy

---
+++ MASsenger.Application/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MASsenger.Application.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(UInt64 entityId);
+        IQueryable<TEntity> GetQueryable();
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ MASsenger.Infrastracture/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using MASsenger.Application.Interfaces;
+using MASsenger.Infrastracture.Data;
+
+namespace MASsenger.Infrastracture.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly MessengerDbContext _context;
+        private readonly DapperDbContext _dapperDbContext;
+        public Repository(MessengerDbContext context, DapperDbContext dapperDbContext)
+        {
+            _context = context;
+            _dapperDbContext = dapperDbContext;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            string query = "SELECT * FROM BaseUsers WHERE Type == 'User'";
+            return (await _dapperDbContext.GetConnection().QueryAsync<TEntity>(query)).ToList();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(UInt64 entityId)
+        {
+            return await _context.Set<TEntity>().FindAsync(entityId);
+        }
+
+        public IQueryable<TEntity> GetQueryable()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+    }
+}
