935187e5bc18e878fa59255bc4b36dec24e1468c
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-08 11:24:13 +0330

    feat: add unit of work
    this class provides a separate ef core SaveChanges method which we can use after performing various operations on different repositories.

---
+++ MASsenger.Infrastracture/Uow/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using MASsenger.Application.Interfaces;
+using MASsenger.Infrastracture.Data;
+
+namespace MASsenger.Infrastracture.Uow
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MessengerDbContext _context;
+
+        public UnitOfWork(MessengerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
