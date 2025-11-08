ee6880867197385a6b3d6554d96644e70058257d
saeed mahjoob <saeed@cloud9p.org>
2025-11-08 11:31:32 +0330

    feat!(dto): add MessageCreateDto.

---
+++ MASsenger.Core/Dto/Create/MessageCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using MASsenger.Core.Entities;
+
+namespace MASsenger.Core.Dto.Create
+{
+    public class MessageCreateDto
+    {
+        public ulong SenderID { set; get; }
+        public ulong DestinationID { set; get; }
+        public string? Text { set; get; }
+        public DateTime? SentTime { set; get; }
+    }
+}
