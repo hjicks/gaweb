8eeed137c4620f03a8d7d03d794fb25490f74064
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-09 21:27:05 +0330

    chore(dto): remove unnecessary dtos

--- MASsenger.Application/Dto/ChannelGroupChatDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MASsenger.Application.Dto
-{
-    public class ChannelGroupChatDto
-    {
-        public String? Name { get; set; } = null;
-        public String? Username { get; set; } = null;
-        public String? Description { get; set; } = null;
-    }
-}
