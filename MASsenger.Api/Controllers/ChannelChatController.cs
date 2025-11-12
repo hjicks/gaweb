2cd5055a577e8a2192f605bb9026851fd2da03cd
Salimiyan <amirali16salimiyan@gmail.com>
2025-11-12 21:05:08 +0330

    feat: add UpdateChannelChatCommand

--- MASsenger.Api/Controllers/ChannelChatController.cs
+++ MASsenger.Api/Controllers/ChannelChatController.cs
@@ -1,5 +1,6 @@
 ﻿using MASsenger.Application.Commands.ChannelChatCommands;
 using MASsenger.Application.Dtos.Create;
+using MASsenger.Application.Dtos.Update;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,22 @@ namespace MASsenger.Api.Controllers
             if (await _sender.Send(new AddChannelChatCommand(channelChat, ownerId)) == Core.Enums.TransactionResultType.Done) return Ok("ChannelChat added successfully.");
             else if (await _sender.Send(new AddChannelChatCommand(channelChat, ownerId)) == Core.Enums.TransactionResultType.ForeignKeyNotFound) return Ok("Invalid Owner Id.");
             return BadRequest("Something went wrong while saving the channelChat.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateChannelChatAsync(ChannelChatUpdateDto channelChat)
+        {
+            if (await _sender.Send(new UpdateChannelChatCommand(channelChat)) == Core.Enums.TransactionResultType.Done) return Ok("ChannelChat updated successfully.");
+            else if (await _sender.Send(new UpdateChannelChatCommand(channelChat)) == Core.Enums.TransactionResultType.ForeignKeyNotFound) return Ok("Invalid channelChat Id.");
+            return BadRequest("Something went wrong while updating the channelChat.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteChannelChatAsync(Int32 channelChatId)
+        {
+            if (await _sender.Send(new DeleteChannelChatCommand(channelChatId)) == Core.Enums.TransactionResultType.Done) return Ok("ChannelChat deleted successfully.");
+            else if (await _sender.Send(new DeleteChannelChatCommand(channelChatId)) == Core.Enums.TransactionResultType.ForeignKeyNotFound) return Ok("Invalid channelChat Id.");
+            return BadRequest("Something went wrong while deleting the channelChat.");
         }
     }
 }
---
+++ MASsenger.Application/Commands/ChannelChatCommands/UpdateChannelChatCommand.cs
@@ -0,0 +1,36 @@
+﻿using MASsenger.Application.Dtos.Update;
+using MASsenger.Application.Interfaces;
+using MASsenger.Core.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Application.Commands.ChannelChatCommands
+{
+    public record UpdateChannelChatCommand(ChannelChatUpdateDto channelChat) : IRequest<TransactionResultType>;
+    public class UpdateChannelChatCommandHandler : IRequestHandler<UpdateChannelChatCommand, TransactionResultType>
+    {
+        private readonly IChannelChatRepository _channelChatRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdateChannelChatCommandHandler(IChannelChatRepository channelChatRepository, IUnitOfWork unitOfWork)
+        {
+            _channelChatRepository = channelChatRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<TransactionResultType> Handle(UpdateChannelChatCommand request, CancellationToken cancellationToken)
+        {
+            var channelChat = await _channelChatRepository.GetByIdAsync(request.channelChat.Id);
+            if (channelChat == null)
+                return TransactionResultType.ForeignKeyNotFound;
+            channelChat.Name = request.channelChat.Name;
+            channelChat.Username = request.channelChat.Username;
+            channelChat.Description = request.channelChat.Description;
+            _channelChatRepository.Update(channelChat);
+            await _unitOfWork.SaveAsync();
+            return TransactionResultType.Done;
+        }
+    }
+}
