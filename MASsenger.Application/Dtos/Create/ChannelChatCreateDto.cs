0fd945bdbf26ca1deae6b60c5c7c19aa1ccf9faa
Salimiyan <amirali16salimiyan@gmail.com>
2025-11-12 20:43:36 +0330

    feat: add ChannelChatDto for Create, Read and Update

---
+++ MASsenger.Application/Dtos/Create/ChannelChatCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Application.Dtos.Create
+{
+    public class ChannelChatCreateDto
+    {
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+    }
+}
