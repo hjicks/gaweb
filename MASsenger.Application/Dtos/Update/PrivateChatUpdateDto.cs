233ea0ae010402bb38a9bc1eb3980cc1860820cf
Salimiyan <amirali16salimiyan@gmail.com>
2025-11-12 22:23:54 +0330

    feat: add PrivateChatDto for Create, Read and Update

---
+++ MASsenger.Application/Dtos/Update/PrivateChatUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Application.Dtos.Update
+{
+    public class PrivateChatUpdateDto
+    {
+        public Int32 Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Description { get; set; }
+    }
+}
