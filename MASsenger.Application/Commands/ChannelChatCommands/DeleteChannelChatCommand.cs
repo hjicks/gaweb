44db84edb1c01f7cded0d12ef91bcbfc61b9e888
Salimiyan <amirali16salimiyan@gmail.com>
2025-11-12 20:59:32 +0330

    feat: add DeleteChannelChatCommand

---
+++ MASsenger.Application/Commands/ChannelChatCommands/DeleteChannelChatCommand.cs
@@ -0,0 +1,33 @@
+﻿using MASsenger.Application.Interfaces;
+using MASsenger.Core.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASsenger.Application.Commands.ChannelChatCommands
+{
+    public record DeleteChannelChatCommand(Int32 channelChatId) : IRequest<TransactionResultType>;
+    public class DeleteChannelChatCommandHandler : IRequestHandler<DeleteChannelChatCommand, TransactionResultType>
+    {
+        private readonly IChannelChatRepository _channelChatRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteChannelChatCommandHandler(IChannelChatRepository channelChatRepository, IUnitOfWork unitOfWork)
+        {
+            _channelChatRepository = channelChatRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<TransactionResultType> Handle(DeleteChannelChatCommand request, CancellationToken cancellationToken)
+        {
+            var channelChat = await _channelChatRepository.GetByIdAsync(request.channelChatId);
+            if (channelChat == null)
+                return TransactionResultType.ForeignKeyNotFound;
+            _channelChatRepository.Delete(channelChat);
+            await _unitOfWork.SaveAsync();
+            return TransactionResultType.Done;
+        }
+    }
+}
