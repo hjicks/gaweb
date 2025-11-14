104249200ac73a8169e0922948c82d55ba9222db
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-14 17:17:27 +0330

    feat: add jwt key in appsettings and import JwtOptions

---
+++ MASsenger.Core/Options/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MASsenger.Core.Options
+{
+    public class JwtOptions
+    {
+        public const string SectionName = "Jwt";
+        public string Key { get; set; } = null!;
+    }
+}
