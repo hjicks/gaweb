9d81bb2e94e3e217032d33a70c21385392037b0c
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-14 20:54:01 +0330

    feat(dto): add validation annotations

--- MASsenger.Application/Dtos/Create/UserCreateDto.cs
+++ MASsenger.Application/Dtos/Create/UserCreateDto.cs
@@ -11,6 +11,8 @@ namespace MASsenger.Application.Dtos.Create
         [MaxLength(32)]
         [RegularExpression("^[a-zA-Z0-9_]+$")]
         public string Username { get; set; } = string.Empty;
+        [MinLength(5)]
+        [MaxLength(32)]
         public string Password { get; set; } = string.Empty;
         [MaxLength(70)]
         public string? Description { get; set; }
