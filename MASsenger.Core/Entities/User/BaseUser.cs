346be56c05421368775969b17cd6d15b719f871f
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-22 00:06:24 +0330

    refactor!(entities): change Username to be required

--- MASsenger.Core/Entities/User/BaseUser.cs
+++ MASsenger.Core/Entities/User/BaseUser.cs
@@ -4,7 +4,7 @@
     {
         public Int32 Id { get; set; }
         public string Name { get; set; } = null!;
-        public string? Username { get; set; }
+        public string Username { get; set; } = null!;
         public string? Description { get; set; } /* acts also as bio */
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public bool IsVerified { get; set; } = false;
