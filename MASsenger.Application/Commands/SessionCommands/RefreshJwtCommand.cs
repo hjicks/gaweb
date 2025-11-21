3cb811e590bda2fa77219b2fd3fbb60f70d58b19
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-22 00:20:03 +0330

    feat(app): import command for refreshing jwt

---
+++ MASsenger.Application/Commands/SessionCommands/RefreshJwtCommand.cs
@@ -0,0 +1,36 @@
+﻿using MASsenger.Application.Interfaces;
+using MediatR;
+using System.Security.Claims;
+
+namespace MASsenger.Application.Commands.SessionCommands
+{
+    public record RefreshJwtCommand(Int32 userId, Guid refreshToken) : IRequest<string>;
+    public class RefreshJwtCommandHandler : IRequestHandler<RefreshJwtCommand, string>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ISessionRepository _sessionRepository;
+        private readonly IJwtService _jwtService;
+        public RefreshJwtCommandHandler(IUserRepository userRepository, ISessionRepository sessionRepository, IJwtService jwtService)
+        {
+            _userRepository = userRepository;
+            _sessionRepository = sessionRepository;
+            _jwtService = jwtService;
+        }
+        public async Task<string> Handle(RefreshJwtCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.userId);
+            if (user == null) { return "Invalid User Id."; }
+            var session = await _sessionRepository.GetActiveSessionByUserIdAsync(request.userId);
+            if (session.Token != request.refreshToken) { return "Invalid Token"; }
+            if (session.ExpiresAt < DateTimeOffset.Now) { return "Token is expired."; }
+            
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+            if (user.Username == "Admin") claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            claims.Add(new Claim(ClaimTypes.Role, "User"));
+            return _jwtService.GetJwt(claims);
+        }
+    }
+}
