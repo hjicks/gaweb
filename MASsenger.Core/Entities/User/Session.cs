777eb28f21a7e7983cce8a18b8161e7f6f32063d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-22 00:01:41 +0330

    feat(entities): import Session entity

---
+++ MASsenger.Core/Entities/User/Session.cs
@@ -0,0 +1,12 @@
+﻿namespace MASsenger.Core.Entities
+{
+    public class Session
+    {
+        public Int32 Id { get; private set; }
+        public Guid Token { get; private set; } = Guid.NewGuid();
+        public DateTimeOffset CreatedAt { get; private set; } = DateTimeOffset.Now;
+        public DateTimeOffset ExpiresAt { get; private set; } = DateTimeOffset.Now.AddDays(7);
+        public User User { get; set; } = null!;
+        public bool IsExpired { get; set; } = false;
+    }
+}
