c1d3dfa4a6c77a3049cab67797d55a277d66e108
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-22 00:22:01 +0330

    feat(api): import SessionController and add endpoint for refreshing jwt

---
+++ MASsenger.Api/Controllers/SessionController.cs
@@ -0,0 +1,30 @@
+﻿using MASsenger.Application.Commands.SessionCommands;
+using MASsenger.Application.Commands.UserCommands;
+using MASsenger.Application.Dtos.Create;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace MASsenger.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private readonly ISender _sender;
+        public SessionController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> RefreshJwt([FromBody] Int32 userId)
+        {
+            Guid refreshToken = Guid.Parse(Request.Cookies["refreshToken"]);
+            var jwt = await _sender.Send(new RefreshJwtCommand(userId, refreshToken));
+            Log.Information($"Jwt of user with id {userId} renewed.");
+            return Ok(jwt);
+        }
+    }
+}
