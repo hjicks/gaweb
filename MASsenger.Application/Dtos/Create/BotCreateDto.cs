0ce4cf8cca1aa0f40ba1838c35964a752e86b884
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-22 21:01:21 +0330

    refactor(app): make bot username required

--- MASsenger.Application/Dtos/Create/BotCreateDto.cs
+++ MASsenger.Application/Dtos/Create/BotCreateDto.cs
@@ -3,7 +3,7 @@
     public class BotCreateDto
     {
         public string Name { get; set; } = null!;
-        public string? Username { get; set; }
+        public string Username { get; set; } = null!;
         public string? Description { get; set; }
         public string Token { get; set; } = null!;
     }
