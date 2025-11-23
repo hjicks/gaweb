de4105736e0a4a65bbc0d1ba037917cd3508fbbc
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-23 19:58:13 +0330

    feat(entities): add fields

--- MASsenger.Core/Entities/Chat/PrivateChat.cs
+++ MASsenger.Core/Entities/Chat/PrivateChat.cs
@@ -2,6 +2,7 @@ namespace MASsenger.Core.Entities
 {
 	public class PrivateChat : BaseChat
 	{
-
+		public BaseUser Starter { get; set; } = null!;
+		public BaseUser Receiver { get; set; } = null!;
 	}
 }
