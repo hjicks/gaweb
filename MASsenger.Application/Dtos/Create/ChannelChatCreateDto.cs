7e0452d5568e7a9e94ae0380234f0783b6c6770d
mhs04 <mahdi.hoseini1381@yahoo.com>
2025-11-23 20:06:03 +0330

    chore(dto): remove unused usings

--- MASsenger.Application/Dtos/Create/ChannelChatCreateDto.cs
+++ MASsenger.Application/Dtos/Create/ChannelChatCreateDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MASsenger.Application.Dtos.Create
+﻿namespace MASsenger.Application.Dtos.Create
 {
     public class ChannelChatCreateDto
     {
