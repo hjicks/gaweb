235c5648ee8bd18308fa94e944581329de82ce2b
saeed mahjoob <saeed@cloud9p.org>
2025-11-30 15:32:30 +0330

    feat: Add a few more tests for validators in LoginCommandTests

--- MASsenger.Test/UnitTests/CommandsTests/SessionCommands/LoginCommandTests.cs
+++ MASsenger.Test/UnitTests/CommandsTests/SessionCommands/LoginCommandTests.cs
@@ -14,12 +14,60 @@ namespace MASsenger.Test.UnitTests.CommandsTests.SessionCommands
         private readonly Mock<ISessionRepository> _sessionRepositoryMock;
         private readonly Mock<IUnitOfWork> _unitOfWorkMock;
         private readonly Mock<IJwtService> _jwtServiceMock;
+        private LoginCommandValidator _validator;
         public LoginCommandTests() 
         {
             _userRepositoryMock = new();
             _sessionRepositoryMock = new();
             _unitOfWorkMock = new();
             _jwtServiceMock = new();
+            _validator = new LoginCommandValidator();
+        }
+
+        [Fact]
+        public async Task Validator_Should_Pass_If_Password_Is_Valid()
+        {
+            // Arrange
+            var userLoginDto = new UserLoginDto()
+            {
+                Username = "Tester",
+                Password = "12345678"
+            };
+            var query = new LoginCommand(userLoginDto);
+
+            var result = await _validator.ValidateAsync(query);
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public async Task Validator_Should_Fail_If_Password_Is_Empty()
+        {
+            // Arrange
+            var userLoginDto = new UserLoginDto()
+            {
+                Username = "Tester",
+                Password = ""
+            };
+            var query = new LoginCommand(userLoginDto);
+
+            var result = await _validator.ValidateAsync(query);
+            Assert.False(result.IsValid);
+        }
+
+
+        [Fact]
+        public async Task Validator_Should_Fail_If_Password_Is_Short()
+        {
+            // Arrange
+            var userLoginDto = new UserLoginDto()
+            {
+                Username = "Tester",
+                Password = "1234"
+            };
+            var query = new LoginCommand(userLoginDto);
+
+            var result = await _validator.ValidateAsync(query);
+            Assert.False(result.IsValid);
         }
 
         [Fact]
