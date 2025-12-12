52e45599c134a6c9e37c2e1ff2ad6d6b31f2c1fd
saeed <saeed@cloud9p.org>
2025-12-12 21:26:00 +0330

    Client: actually import the client, it seems i have actually forget to include it. sorry.
    see the message for previous commit's previous commit for more info on how to use this client.

--- MASsenger.Client/Program.cs
+++ MASsenger.Client/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connectionSignalR = new HubConnectionBuilder().WithUrl("https://localhost:7088/hub").Build();
+connectionSignalR.StartAsync().Wait();
+
+Console.WriteLine("Connected, waiting for events...");
+while (true)
+{
+    Thread.Sleep(1000);
+    connectionSignalR.On<string>("RecieveText", text => Console.WriteLine(text));
+}
