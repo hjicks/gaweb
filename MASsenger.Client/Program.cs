d87595738f38570e5ef632da29e467097429ddf0
saeed <saeed@cloud9p.org>
2025-12-14 00:30:51 +0330

    feat(client): Obtain JWT and listen to events as well

--- MASsenger.Client/Program.cs
+++ MASsenger.Client/Program.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
 
-var connectionSignalR = new HubConnectionBuilder().WithUrl("https://localhost:7088/hub").Build();
+using Microsoft.AspNetCore.SignalR.Client;
+
+string baseurl = "https://localhost:7088";
+JsonElement Login(string username, string password)
+{
+    HttpClient c = new HttpClient { BaseAddress = new Uri(baseurl) };
+    var loginData = new { username = username, password = password };
+    var response = c.PostAsJsonAsync("/api/session/login", loginData).Result
+        .Content.ReadAsStringAsync().Result;
+    return JsonSerializer.Deserialize<JsonElement>(response);
+}
+Console.WriteLine("Authenticating...");
+JsonElement r = Login("Admin", "sysadmin");
+String jwt = r.GetProperty("response").GetProperty("jwt").ToString();
+Console.WriteLine($"Auth successed..., jwt token is {jwt}");
+Console.WriteLine("Connecting to the hub, waiting for events...");
+var connectionSignalR = new HubConnectionBuilder().WithUrl(baseurl + "/hub", options =>
+    {
+        options.AccessTokenProvider = () => Task.FromResult(jwt);
+    }).Build();
 connectionSignalR.StartAsync().Wait();
 
-Console.WriteLine("Connected, waiting for events...");
 while (true)
 {
     Thread.Sleep(1000);
-    connectionSignalR.On<string>("RecieveText", text => Console.WriteLine(text));
+    connectionSignalR.On<string, string>("a", (text, text2) => Console.WriteLine(text+text2));
 }
