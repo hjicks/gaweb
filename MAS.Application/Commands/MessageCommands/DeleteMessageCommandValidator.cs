af755dded7d6a27d923d7f3962bcab7890636b76
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-23 00:09:10 +0330

    feat: Add DeleteMessageCommandValidator

---
+++ MAS.Application/Commands/MessageCommands/DeleteMessageCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MAS.Application.Commands.SessionCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS.Application.Commands.MessageCommands
+{
+    public class DeleteMessageCommandValidator : AbstractValidator<DeleteMessageCommand>
+    {
+        public DeleteMessageCommandValidator() 
+        {
+            RuleFor(c => c.MessageId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("MessageId is required.")
+                .GreaterThan(0)
+                .WithMessage("MessageId must be greater than zero.");
+        }
+    }
+}
