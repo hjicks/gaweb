e684e629a661d335a7537ae65cfef64163e45899
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 14:09:37 +0330

    refactor: Modify DeletePrivateChatCommandValidator

--- MAS.Application/Commands/PrivateChatCommands/DeletePrivateChatCommandValidator.cs
+++ MAS.Application/Commands/PrivateChatCommands/DeletePrivateChatCommandValidator.cs
@@ -1,22 +1,16 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MAS.Application.Commands.PrivateChatCommands
+namespace MAS.Application.Commands.PrivateChatCommands;
+
+public class DeletePrivateChatCommandValidator : AbstractValidator<DeletePrivateChatCommand>
 {
-    public class DeletePrivateChatCommandValidator : AbstractValidator<DeletePrivateChatCommand>
+    public DeletePrivateChatCommandValidator() 
     {
-        public DeletePrivateChatCommandValidator() 
-        {
-            RuleFor(c => c.PrivateChatId)
-                .Cascade(CascadeMode.Stop)
-                .NotEmpty()
-                .WithMessage("PrivateChatId is required.")
-                .GreaterThan(0)
-                .WithMessage("PrivateChatId must be greater than zero.");
-        }
+        RuleFor(c => c.PrivateChatId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .WithMessage("PrivateChatId is required.")
+            .GreaterThan(0)
+            .WithMessage("PrivateChatId must be greater than zero.");
     }
 }
