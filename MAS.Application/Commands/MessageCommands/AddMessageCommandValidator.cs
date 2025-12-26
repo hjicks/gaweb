4a9e3e04fc408687de203216ef59c6a17911258c
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 14:04:01 +0330

    refactor: Modify MeeageCommands Validators

--- MAS.Application/Commands/MessageCommands/AddMessageCommandValidator.cs
+++ MAS.Application/Commands/MessageCommands/AddMessageCommandValidator.cs
@@ -1,34 +1,28 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MAS.Application.Commands.MessageCommands
+namespace MAS.Application.Commands.MessageCommands;
+
+public class AddMessageCommandValidator : AbstractValidator<AddMessageCommand>
 {
-    public class AddMessageCommandValidator : AbstractValidator<AddMessageCommand>
+    public AddMessageCommandValidator() 
     {
-        public AddMessageCommandValidator() 
-        {
-            RuleFor(c => c.Message.DestinationId)
-                .Cascade(CascadeMode.Stop)
-                .NotEmpty()
-                .WithMessage("DestinationId is required.")
-                .GreaterThan(0)
-                .WithMessage("DestinationId must be greater than zero.");
+        RuleFor(c => c.Message.DestinationId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .WithMessage("DestinationId is required.")
+            .GreaterThan(0)
+            .WithMessage("DestinationId must be greater than zero.");
 
-            RuleFor(c => c.Message.FileName)
-                .MaximumLength(32)
-                .WithMessage("FileName cannot exceed 32 characters.");
+        RuleFor(c => c.Message.FileName)
+            .MaximumLength(32)
+            .WithMessage("FileName cannot exceed 32 characters.");
 
-            RuleFor(c => c.Message.FileSize)
-                .GreaterThan(0UL)
-                .WithMessage("FileSize must be greater than zero.");
+        RuleFor(c => c.Message.FileSize)
+            .GreaterThan(0UL)
+            .WithMessage("FileSize must be greater than zero.");
 
-            RuleFor(c => c.Message.FileContentType)
-                .Length(5,32)
-                .WithMessage("FileContentType must be between 5 and 32 characters.");
-        }
+        RuleFor(c => c.Message.FileContentType)
+            .Length(5,32)
+            .WithMessage("FileContentType must be between 5 and 32 characters.");
     }
 }
