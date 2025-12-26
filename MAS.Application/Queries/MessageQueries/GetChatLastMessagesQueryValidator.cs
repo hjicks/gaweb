faa54315ceb915741b46375dc79cbdafac7ba55d
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 13:51:58 +0330

    feat: Add GetChatLastMessagesQueryValidator

---
+++ MAS.Application/Queries/MessageQueries/GetChatLastMessagesQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MAS.Application.Queries.MessageQueries;
+
+public class GetChatLastMessagesQueryValidator : AbstractValidator<GetChatLastMessagesQuery>
+{
+    public GetChatLastMessagesQueryValidator()
+    {
+        RuleFor(g => g.MessageId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("MessageId is required.")
+                .GreaterThan(0)
+                .WithMessage("MessageId must be greater than zero.");
+    }
+}
