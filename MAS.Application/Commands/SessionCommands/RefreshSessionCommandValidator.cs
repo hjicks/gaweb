57b409a6d7fd7d3ab2e5f1c1982b9751624a8434
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 19:14:39 +0330

    feat: Add RefreshSessionCommandValidator

---
+++ MAS.Application/Commands/SessionCommands/RefreshSessionCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MAS.Application.Commands.SessionCommands;
+
+public class RefreshSessionCommandValidator : AbstractValidator<RefreshSessionCommand>
+{
+    public RefreshSessionCommandValidator()
+    {
+        RuleFor(c => c.TokenDto.RefreshToken)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .WithMessage("RefreshToken is required.")
+            .Length(44)
+            .WithMessage("RefreshToken must be exactly 44 characters long.");
+    }
+}
