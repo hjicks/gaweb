3265b5b3f086fe373b1d3be27304c8b2a7713879
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 13:40:31 +0330

    feat: Add GetGroupChatMembersQueryValidator

---
+++ MAS.Application/Queries/GroupChatQueries/GetGroupChatMembersQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MAS.Application.Queries.GroupChatQueries;
+
+public class GetGroupChatMembersQueryValidator : AbstractValidator<GetGroupChatMembersQuery>
+{
+    public GetGroupChatMembersQueryValidator()
+    {
+        RuleFor(g => g.GroupChatId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("GroupChatId is required.")
+                .GreaterThan(0)
+                .WithMessage("GroupChatId must be greater than zero.");
+    }
+}
