c2561cf5a3bc0fc9bf2d1497454634c0b6d620a1
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 12:30:00 +0330

    feat: Add LeaveGroupChatCommandValidator

---
+++ MAS.Application/Commands/GroupChatCommands/LeaveGroupChatCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace MAS.Application.Commands.GroupChatCommands;
+
+public class LeaveGroupChatCommandValidator : AbstractValidator<LeaveGroupChatCommand>
+{
+    public LeaveGroupChatCommandValidator()
+    {
+        RuleFor(g => g.GroupChatId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("GroupChatId is required.")
+                .GreaterThan(0)
+                .WithMessage("GroupChatId must be greater than zero.");
+    }
+}
