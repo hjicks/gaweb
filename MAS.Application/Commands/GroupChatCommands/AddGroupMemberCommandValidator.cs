aa67762d3f0d75f4de92902f0fd8870ac0bda8bf
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 11:10:03 +0330

    feat: AddGroupMemberCommandValidator

---
+++ MAS.Application/Commands/GroupChatCommands/AddGroupMemberCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MAS.Application.Commands.GroupChatCommands;
+
+public class AddGroupMemberCommandValidator : AbstractValidator<AddGroupMemberCommand>
+{
+    public AddGroupMemberCommandValidator()
+    {
+        RuleFor(g => g.GroupChatId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("GroupChatId is required.")
+                .GreaterThan(0)
+                .WithMessage("GroupChatId must be greater than zero.");
+
+        RuleFor(g => g.MemberId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("MemberId is required.")
+                .GreaterThan(0)
+                .WithMessage("MemberId must be greater than zero.");
+    }
+}
