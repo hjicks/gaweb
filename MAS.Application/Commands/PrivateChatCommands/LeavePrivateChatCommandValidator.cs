7157121dd0830f8ed1a8c7b823bbe8ee2af626a8
Salimiyan <amirali16salimiyan@gmail.com>
2025-12-26 14:20:08 +0330

    feat: Add LeavePrivateChatCommandValidator

---
+++ MAS.Application/Commands/PrivateChatCommands/LeavePrivateChatCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MAS.Application.Commands.GroupChatCommands;
+
+namespace MAS.Application.Commands.PrivateChatCommands;
+
+public class LeavePrivateChatCommandValidator : AbstractValidator<LeavePrivateChatCommand>
+{
+    public LeavePrivateChatCommandValidator()
+    {
+        RuleFor(c => c.PrivateChatId)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .WithMessage("PrivateChatId is required.")
+            .GreaterThan(0)
+            .WithMessage("PrivateChatId must be greater than zero.");
+    }
+}
